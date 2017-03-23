d7fa97226ac43af57766e356874c55b861b1ae8a
Amosh Gurung <ag00556@surrey.ac.uk>
2017-03-23 12:03:36 +0000

    Merge branch 'master' of D:\Android_Projects\Group03 with conflicts.

--- src/src/main/java/com/com2027/group03/MainActivity.java
+++ src/src/main/java/com/com2027/group03/MainActivity.java
@@ -3,6 +3,8 @@ package com.com2027.group03;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button help = (Button) findViewById(R.id.helpButton);
+
+        assert help != null;
+        help.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this,HelpPopUp.class));
+            }
+        });
+
 
         //Intent intent = new Intent(this, TestSpritesActivity.class);
         //startActivity(intent);
