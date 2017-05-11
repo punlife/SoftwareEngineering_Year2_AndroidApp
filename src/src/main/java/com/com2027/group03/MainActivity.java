f61b8c771cad06b6abdf083ee8637ef209fad5f2
amoshgurung <ag00556@surrey.ac.uk>
2017-05-11 00:43:20 +0000

    - Update Android Manifest - Updated Main-Activity and main_activity layout file - Added few icon on drawables

--- src/src/main/java/com/com2027/group03/MainActivity.java
+++ src/src/main/java/com/com2027/group03/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,23 +14,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button help = (Button) findViewById(R.id.helpButton);
+        //Intent intent = new Intent(this, TestSpritesActivity.class);
+        //startActivity(intent);
 
-        assert help != null;
-        help.setOnClickListener(new View.OnClickListener() {
+        //Button to open help screen.
+        ImageView help_iw = (ImageView)findViewById(R.id.help_button);
+        help_iw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this,HelpPopUp.class));
+                Intent intentHelpScreen = new Intent(MainActivity.this, HelpScreen.class);
+                startActivity(intentHelpScreen);
             }
         });
 
 
-        //Intent intent = new Intent(this, TestSpritesActivity.class);
-        //startActivity(intent);
+        ImageView setting_iw = (ImageView)findViewById(R.id.settings_button);
 
-        Intent intent = new Intent(this, CardsActivity.class);
-        intent.putExtra("cardsPerRow", 3); // 3 cards per row, therefore 2 cards per column
-        intent.putExtra("initialShowDelay", 6000); // 3 seconds
-        startActivity(intent);
+        //Button to start the game.
+        Button play_b = (Button)findViewById(R.id.play_button);
+        play_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CardsActivity.class);
+                intent.putExtra("cardsPerRow", 3); // 3 cards per row, therefore 2 cards per column
+                intent.putExtra("initialShowDelay", 6000); // 3 seconds
+                startActivity(intent);
+            }
+        });
+        Button highscore_b = (Button)findViewById(R.id.highScore_button);
+
+        //Button to start Contact Page.
+        Button contactUs_b = (Button)findViewById(R.id.contactUs_button);
+        contactUs_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent contactUs = new Intent(MainActivity.this, ContactPage.class);
+                startActivity(contactUs);
+            }
+        });
+
     }
 }
