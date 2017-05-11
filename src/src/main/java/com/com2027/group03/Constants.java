cb1d694116ea49c0f97711d190553ae33ffba805
akhil <aa01538@surrey.ac.uk>
2017-05-11 10:38:39 +0100

    - Added LoginActivity Class - Added RegisterActivity class - Added SharedPrefManager Class - Added RequestHandler Class - Added ProfileActivity Class

---
+++ src/src/main/java/com/com2027/group03/Constants.java
@@ -0,0 +1,15 @@
+package com.com2027.group03;
+
+/**
+ *
+ */
+
+public class Constants {
+
+    private static final String ROOT_URL = "http://192.168.1.101/Android/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+
+
+}
---
+++ src/src/main/java/com/com2027/group03/RegisterActivity.java
@@ -0,0 +1,103 @@
+package com.com2027.group03;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private EditText editTextUsername, editTextEmail, editTextPassword;
+    private Button buttonRegister;
+    private ProgressDialog progressDialog;
+
+    private TextView textViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(SharedPrefManager.getInstance(this).isLoggedIn()){
+            finish();
+            startActivity(new Intent(this, ProfileActivity.class));
+            return;
+        }
+
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        textViewLogin = (TextView) findViewById(R.id.textViewLogin);
+
+        buttonRegister = (Button) findViewById(R.id.buttonRegister);
+
+        progressDialog = new ProgressDialog(this);
+
+        buttonRegister.setOnClickListener(this);
+        textViewLogin.setOnClickListener(this);
+    }
+
+    private void registerUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        final String username = editTextUsername.getText().toString().trim();
+        final String password = editTextPassword.getText().toString().trim();
+
+        //todo implement based on victor's server
+
+        /*progressDialog.setMessage("Registering user...");
+        progressDialog.show();
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,
+                Constants.URL_REGISTER,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        progressDialog.dismiss();
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+
+                            Toast.makeText(getApplicationContext(), "Message2: " + jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        progressDialog.hide();
+                        Toast.makeText(getApplicationContext(), "Error2: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("username", username);
+                params.put("email", email);
+                params.put("password", password);
+                return params;
+            }
+        };
+
+
+        RequestHandler.getInstance(this).addToRequestQueue(stringRequest);*/
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == buttonRegister)
+            registerUser();
+        if(view == textViewLogin)
+            startActivity(new Intent(this, LoginActivity.class));
+    }
+}
