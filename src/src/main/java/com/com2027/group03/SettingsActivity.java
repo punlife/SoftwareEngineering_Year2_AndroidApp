0416d11a8b6fa4831c8408258bf6a4a5cbbd9338
Collaki R Miss (UG - Computer Science) <rc00389@surrey.ac.uk>
2017-05-12 15:20:45 +0100

    - added comments to five files - deleted unnecessary strings in strings.xml

--- src/src/main/java/com/com2027/group03/SettingsActivity.java
+++ src/src/main/java/com/com2027/group03/SettingsActivity.java
@@ -233,7 +233,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_data_sync);
             setHasOptionsMenu(true);
-
             // Bind the summaries of EditText/List/Dialog/Ringtone preferences
             // to their values. When their values change, their summaries are
             // updated to reflect the new value, per the Android Design
