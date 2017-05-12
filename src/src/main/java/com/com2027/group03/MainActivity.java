ca3ce13b78e3b0d5182a007c5d33a605ef183c03
LUKASZDROZDA\Lukasz_Drozda <ld00245@surrey.ac.uk>
2017-05-12 12:17:06 +0100

    -Minor changes to layout, colours and new logo added

--- src/src/main/java/com/com2027/group03/MainActivity.java
+++ src/src/main/java/com/com2027/group03/MainActivity.java
@@ -1,9 +1,11 @@
 package com.com2027.group03;
 
 import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -13,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
 
         //Intent intent = new Intent(this, TestSpritesActivity.class);
         //startActivity(intent);
